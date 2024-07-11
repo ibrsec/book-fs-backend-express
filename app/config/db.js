ce463e4d099bea6123bb0751ebba49950366107d
ibr <ibr.seckin@gmail.com>
2024-07-11 07:13:02 +0300

    pg fix

--- app/config/db.js
+++ app/config/db.js
@@ -1,6 +1,17 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize(process.env.CONNECTION_STRING);
+const sequelize = new Sequelize(process.env.CONNECTION_STRING, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true, // SSL gereksinimini ayarlayın
+        rejectUnauthorized: false // Sertifikayı doğrulamayın
+      }
+    },
+    dialectModule: require('pg'),
+  });
 
 
 module.exports = sequelize;
